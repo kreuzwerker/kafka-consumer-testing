aeefd6d1ead697d82f023ae3b193c929606caebf
Anja Gruss <anja.gruss@kreuzwerker.de>
2021-05-14 10:40:09 +0200

    cleanup

--- src/test/java/de/kreuzwerker/blogs/kafkaconsumertesting/PostgresAndKafkaTestCase.java
+++ src/test/java/de/kreuzwerker/blogs/kafkaconsumertesting/PostgresAndKafkaTestCase.java
@@ -29,96 +29,96 @@ import java.util.stream.Stream;
 @Slf4j
 @ContextConfiguration(initializers = {PostgresAndKafkaTestCase.Initializer.class})
 public class PostgresAndKafkaTestCase {
-    protected static Network network = Network.newNetwork();
+  protected static Network network = Network.newNetwork();
 
-    protected static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:10.9")
-                    .withPassword("postgres")
-                    .withUsername("postgres")
-                    .withExposedPorts(5432)
-                    .withReuse(true);
+  protected static final PostgreSQLContainer<?> postgreSQLContainer =
+      new PostgreSQLContainer<>("postgres:10.9")
+          .withPassword("postgres")
+          .withUsername("postgres")
+          .withExposedPorts(5432)
+          .withReuse(true);
 
-    protected static final KafkaContainer kafkaContainer =
-            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.5.3"))
-                    .withEmbeddedZookeeper()
-                    .withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9093 ,BROKER://0.0.0.0:9092")
-                    .withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "BROKER:PLAINTEXT,PLAINTEXT:PLAINTEXT")
-                    .withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER")
-                    .withEnv("KAFKA_BROKER_ID", "1")
-                    .withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
-                    .withEnv("KAFKA_OFFSETS_TOPIC_NUM_PARTITIONS", "1")
-                    .withEnv("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1")
-                    .withEnv("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1")
-                    .withEnv("KAFKA_LOG_FLUSH_INTERVAL_MESSAGES", Long.MAX_VALUE + "")
-                    .withEnv("KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS", "0")
-                    .withNetwork(network);
+  protected static final KafkaContainer kafkaContainer =
+      new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.5.3"))
+          .withEmbeddedZookeeper()
+          .withEnv("KAFKA_LISTENERS", "PLAINTEXT://0.0.0.0:9093 ,BROKER://0.0.0.0:9092")
+          .withEnv("KAFKA_LISTENER_SECURITY_PROTOCOL_MAP", "BROKER:PLAINTEXT,PLAINTEXT:PLAINTEXT")
+          .withEnv("KAFKA_INTER_BROKER_LISTENER_NAME", "BROKER")
+          .withEnv("KAFKA_BROKER_ID", "1")
+          .withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1")
+          .withEnv("KAFKA_OFFSETS_TOPIC_NUM_PARTITIONS", "1")
+          .withEnv("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1")
+          .withEnv("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1")
+          .withEnv("KAFKA_LOG_FLUSH_INTERVAL_MESSAGES", Long.MAX_VALUE + "")
+          .withEnv("KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS", "0")
+          .withNetwork(network);
 
-    static {
-        Startables.deepStart(Stream.of(postgreSQLContainer, kafkaContainer)).join();
+  static {
+    Startables.deepStart(Stream.of(postgreSQLContainer, kafkaContainer)).join();
+  }
+
+  public static class Initializer
+      implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+      TestPropertyValues.of(
+              "spring.datasource.driver-class-name=" + postgreSQLContainer.getDriverClassName(),
+              "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+              "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+              "spring.datasource.password=" + postgreSQLContainer.getPassword(),
+              "spring.jpa.properties.hibernate.default_schema=public",
+              "spring.jpa.show-sql=true",
+              "spring.liquibase.drop-first=true",
+              "spring.liquibase.change-log=classpath:db/liquibase-changelog.xml",
+              "spring.liquibase.default-schema=public",
+              "spring.liquibase.liquibase-schema=public",
+              "spring.kafka.bootstrap-servers=" + kafkaContainer.getBootstrapServers(),
+              "spring.kafka.client-id=kafkatest",
+              "spring.kafka.consumer.group-id=kafkatest",
+              "spring.kafka.consumer.auto-offset-reset=earliest",
+              "messaging.enabled=true")
+          .applyTo(configurableApplicationContext.getEnvironment());
     }
+  }
 
-    public static class Initializer
-            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+  public static KafkaProducer<Object, Object> createProducer() {
+    Properties props = new Properties();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
+    props.put(ProducerConfig.ACKS_CONFIG, "all");
+    props.put(ProducerConfig.RETRIES_CONFIG, 0);
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    props.put(
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+    props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "mock://testUrl");
+    props.put(ProducerConfig.CLIENT_ID_CONFIG, "kafkatest");
+    return new KafkaProducer<>(props);
+  }
 
-        @Override
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                    "spring.datasource.driver-class-name=" + postgreSQLContainer.getDriverClassName(),
-                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                    "spring.datasource.password=" + postgreSQLContainer.getPassword(),
-                    "spring.jpa.properties.hibernate.default_schema=public",
-                    "spring.jpa.show-sql=true",
-                    "spring.liquibase.drop-first=true",
-                    "spring.liquibase.change-log=classpath:db/liquibase-changelog.xml",
-                    "spring.liquibase.default-schema=public",
-                    "spring.liquibase.liquibase-schema=public",
-                    "spring.kafka.bootstrap-servers=" + kafkaContainer.getBootstrapServers(),
-                    "spring.kafka.client-id=kafkatest",
-                    "spring.kafka.consumer.group-id=kafkatest",
-                    "spring.kafka.consumer.auto-offset-reset=earliest",
-                    "messaging.enabled=true")
-                    .applyTo(configurableApplicationContext.getEnvironment());
-        }
-    }
+  public static KafkaConsumer<String, Object> createEventConsumer() {
+    Properties props = new Properties();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
+    props.put(
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+        io.confluent.kafka.serializers.KafkaAvroDeserializer.class);
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "mock://testUrl");
+    props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "kafkatest");
+    return new KafkaConsumer<>(props);
+  }
 
-    public static KafkaProducer<Object, Object> createProducer() {
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
-        props.put(ProducerConfig.ACKS_CONFIG, "all");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        //props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, MockAvroSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroSerializer.class);
-        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "mock://testUrl");
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "kafkatest");
-        return new KafkaProducer<>(props);
-    }
+  public static void sendEvent(KafkaProducer producer, ProducerRecord record)
+      throws ExecutionException, InterruptedException {
 
-    public static KafkaConsumer<String, Object> createEventConsumer() {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
-        props.put(
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                //de.kreuzwerker.blogs.kafkaconsumertesting.messaging.MockAvroDeserializer.class
-                io.confluent.kafka.serializers.KafkaAvroDeserializer.class);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "mock://testUrl");
-        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "kafkatest");
-        return new KafkaConsumer<>(props);
-    }
+    Future sendFuture = producer.send(record);
 
-    public static void sendEvent(KafkaProducer producer, ProducerRecord record)
-            throws ExecutionException, InterruptedException {
-
-        Future sendFuture = producer.send(record);
-
-        RecordMetadata metadata = (RecordMetadata) sendFuture.get();
-        log.info(
-                "RecordMetadata topic: {}, offset: {}, partition: {}",
-                metadata.topic(),
-                metadata.offset(),
-                metadata.partition());
-    }
+    RecordMetadata metadata = (RecordMetadata) sendFuture.get();
+    log.info(
+        "RecordMetadata topic: {}, offset: {}, partition: {}",
+        metadata.topic(),
+        metadata.offset(),
+        metadata.partition());
+  }
 }
